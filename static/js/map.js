53b9994e6b61ad3e85a50cd5ceaf74a26324798c
Andrey <iHenk0@yandex.ru>
2020-05-06 19:42:07 +0300

    fix: Наши кнопки зума

--- static/js/map.js
+++ static/js/map.js
@@ -1,68 +1,72 @@
 //let info = axios.GET().then((response) => response.data)
-
-ymaps.ready(init);
+var myMap;
 
 function init() {
-    var myMap = new ymaps.Map("map", {
-            center: [55.75, 37.61],
-            zoom: 10,
-            controls: [],
-        }),
-        // Создадим пользовательский макет ползунка масштаба.
-        ZoomLayout = ymaps.templateLayoutFactory.createClass(
-            "<div>" +
-                "<div id='zoom-in' class='btn' style='margin-top: 200px'>+</div><br>" +
-                "<div id='zoom-out' class='btn'>-</div>" +
-                "</div>",
-            {
-                // Переопределяем методы макета, чтобы выполнять дополнительные действия
-                // при построении и очистке макета.
-                build: function () {
-                    // Вызываем родительский метод build.
-                    ZoomLayout.superclass.build.call(this);
+    myMap = new ymaps.Map("map", {
+        center: [55.75, 37.61],
+        zoom: 10,
+        controls: [],
+    });
 
-                    // Привязываем функции-обработчики к контексту и сохраняем ссылки
-                    // на них, чтобы потом отписаться от событий.
-                    this.zoomInCallback = ymaps.util.bind(this.zoomIn, this);
-                    this.zoomOutCallback = ymaps.util.bind(this.zoomOut, this);
+    // Создадим пользовательский макет ползунка масштаба.
+    let ZoomLayout = ymaps.templateLayoutFactory.createClass(
+        "<ul class='zoom'>" +
+            "<li id='zoom-in'>" +
+            "<button>+</button>" +
+            "</li>" +
+            "<li id='zoom-out'>" +
+            "<button>&#8722;</button>" +
+            "</li>" +
+            "</ul>",
+        {
+            // Переопределяем методы макета, чтобы выполнять дополнительные действия
+            // при построении и очистке макета.
+            build: function () {
+                // Вызываем родительский метод build.
+                ZoomLayout.superclass.build.call(this);
 
-                    // Начинаем слушать клики на кнопках макета.
-                    $("#zoom-in").bind("click", this.zoomInCallback);
-                    $("#zoom-out").bind("click", this.zoomOutCallback);
-                },
+                // Привязываем функции-обработчики к контексту и сохраняем ссылки
+                // на них, чтобы потом отписаться от событий.
+                this.zoomInCallback = ymaps.util.bind(this.zoomIn, this);
+                this.zoomOutCallback = ymaps.util.bind(this.zoomOut, this);
 
-                clear: function () {
-                    // Снимаем обработчики кликов.
-                    $("#zoom-in").unbind("click", this.zoomInCallback);
-                    $("#zoom-out").unbind("click", this.zoomOutCallback);
+                // Начинаем слушать клики на кнопках макета.
+                $("#zoom-in").bind("click", this.zoomInCallback);
+                $("#zoom-out").bind("click", this.zoomOutCallback);
+            },
 
-                    // Вызываем родительский метод clear.
-                    ZoomLayout.superclass.clear.call(this);
-                },
+            clear: function () {
+                // Снимаем обработчики кликов.
+                $("#zoom-in").unbind("click", this.zoomInCallback);
+                $("#zoom-out").unbind("click", this.zoomOutCallback);
 
-                zoomIn: function () {
-                    var map = this.getData().control.getMap();
-                    map.setZoom(map.getZoom() + 1, { checkZoomRange: true });
-                },
+                // Вызываем родительский метод clear.
+                ZoomLayout.superclass.clear.call(this);
+            },
 
-                zoomOut: function () {
-                    var map = this.getData().control.getMap();
-                    map.setZoom(map.getZoom() - 1, { checkZoomRange: true });
-                },
-            }
-        ),
-        zoomControl = new ymaps.control.ZoomControl({ options: { layout: ZoomLayout } });
+            zoomIn: function () {
+                var map = this.getData().control.getMap();
+                map.setZoom(map.getZoom() + 1, { checkZoomRange: true });
+            },
+
+            zoomOut: function () {
+                var map = this.getData().control.getMap();
+                map.setZoom(map.getZoom() - 1, { checkZoomRange: true });
+            },
+        }
+    );
+    let zoomControl = new ymaps.control.ZoomControl({ options: { layout: ZoomLayout } });
     myMap.controls.add(zoomControl);
 
     // Создание точек
-    vm.info.data.forEach((film) => {
+    vm.info.points.forEach((point) => {
         myMap.geoObjects.add(
             new ymaps.Placemark(
                 // Координаты
-                [+film.point.lat, +film.point.lon],
+                [+point.lat, +point.lon],
                 // Контент
                 {
-                    balloonContent: film.movie.name,
+                    balloonContent: "-",
                 },
                 {
                     preset: "islands#dotIcon",
